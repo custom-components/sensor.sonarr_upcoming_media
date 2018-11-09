524eb4c5f677743d9ea0689b80007070bfa8912e
MKR <25127328+maykar@users.noreply.github.com>
2018-11-09 07:54:21 -0500

    Add days_until() and refactor/format

--- custom_components/sensor/sonarr_upcoming_media.py
+++ custom_components/sensor/sonarr_upcoming_media.py
@@ -7,9 +7,12 @@ https://github.com/custom-components/sensor.radarr_upcoming_media
 https://github.com/custom-cards/upcoming-media-card
 
 """
-import logging, requests, time, re, json
+import logging
+import json
+import time
 from datetime import date, datetime
-import voluptuous as vol, homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_API_KEY, CONF_HOST, CONF_PORT, CONF_SSL
 from homeassistant.helpers.entity import Entity
@@ -49,7 +52,7 @@ class SonarrUpcomingMediaSensor(Entity):
         self._state = None
         self.data = []
         self._tz = timezone(str(hass.config.time_zone))
-        self.attribNum = 0
+        self.item_count = 0
         self.now = str(get_date(self._tz))
         self.max_items = int(conf.get(CONF_MAX))
 
@@ -63,8 +66,9 @@ class SonarrUpcomingMediaSensor(Entity):
 
     @property
     def device_state_attributes(self):
-        """Build JSON for the sensor."""
-        self.attribNum = 0
+        import re
+        """Return JSON for the sensor."""
+        self.item_count = 0
         attributes = {}
         default = {}
         data = []
@@ -77,66 +81,96 @@ class SonarrUpcomingMediaSensor(Entity):
         data.append(default)
         for show in self.data:
             pre = {}
-            try: pre['airdate'] = show['airDateUtc']
-            except: continue
-            try:
-                """Get number of days between now and air date."""
-                n=list(map(int, self.now.split("-")))
-                r=list(map(int, show['airDateUtc'][:-10].split("-")))
-                today = date(n[0],n[1],n[2])
-                airday = date(r[0],r[1],r[2])
-                daysBetween = (airday-today).days
-            except: continue
-            if daysBetween <= 7: pre['release'] = '$day, $time'
-            else: pre['release'] = '$day, $date $time'
+            if 'series' not in show:
+                continue
+            pre['airdate'] = show['airDateUtc']
+            if days_until(show['airDateUtc'], self._tz) <= 7:
+                pre['release'] = '$day, $time'
+            else:
+                pre['release'] = '$day, $date $time'
             pre['flag'] = show.get('hasFile', False)
-            try: pre['title'] = show['series']['title']
-            except: continue
+            if 'title' in show['series']:
+                pre['title'] = show['series']['title']
+            else:
+                continue
             pre['episode'] = show.get('title','')
-            try: pre['number'] = 'S{:02d}E{:02d}'.format(show['seasonNumber'], show['episodeNumber'])
-            except: pre['number'] = 0
-            try: pre['runtime'] = show['series']['runtime']
-            except: pre['runtime'] = 0
-            try: pre['studio'] = show['series']['network']
-            except: pre['studio'] = 0
-            try:
-                if show['series']['ratings']['value'] > 0:
-                    pre['rating'] = '\N{BLACK STAR} '+str(show['series']['ratings']['value'])
-                else: pre['rating'] = ''
-            except: pre['rating'] = ''
-            try: pre['genres'] = ', '.join(show['series']['genres'])
-            except: pre['genres'] = ''
-            try: pre['poster'] = re.sub('banners/', 'banners/_cache/', show['series']['images'][2]['url'])
-            except: continue
+            if 'seasonNumber' and 'episodeNumber' in show:
+                pre['number'] = 'S{:02d}E{:02d}'.format(show['seasonNumber'],
+                                                        show['episodeNumber'])
+            else: pre['number'] = ''
+            if 'runtime' in show['series']:
+                pre['runtime'] = show['series']['runtime']
+            else:
+                pre['runtime'] = ''
+            if 'network' in show['series']:
+                pre['studio'] = show['series']['network']
+            else:
+                pre['studio'] = 0
+            if ('ratings' in show ['series'] and 
+                show['series']['ratings']['value'] > 0):
+                    pre['rating'] = ('\N{BLACK STAR} ' +
+                                     str(show['series']['ratings']['value']))
+            else:
+                pre['rating'] = ''
+            if 'genres' in show['series']:
+                pre['genres'] = ', '.join(show['series']['genres'])
+            else:
+                pre['genres'] = ''
+            try: pre['poster'] = re.sub('banners/', 'banners/_cache/',
+                                        show['series']['images'][2]['url'])
+            except:
+                continue
             try:
                 if '.jpg' in show['series']['images'][0]['url']:
-                    pre['fanart'] = re.sub('banners/', 'banners/_cache/', show['series']['images'][0]['url'])
-                else: pre['fanart'] = ''
-            except: pre['fanart'] = ''
-            self.attribNum += 1
+                    pre['fanart'] = re.sub('banners/', 'banners/_cache/',
+                                            show['series']['images'][0]['url'])
+                else:
+                    pre['fanart'] = ''
+            except:
+                pre['fanart'] = ''
+            self.item_count += 1
             data.append(pre)
-        self._state = self.attribNum
+        self._state = self.item_count
         attributes['data'] = json.dumps(data)
         return attributes
 
     def update(self):
+        import requests
         start = get_date(self._tz)
         end = get_date(self._tz, self.days)
         try:
-            res = requests.get('http{0}://{1}:{2}/{3}api/calendar?start={4}&end={5}'.format(
-                    self.ssl, self.host, self.port, self.urlbase, start, end),
+            api = requests.get('http{0}://{1}:{2}/{3}api/calendar?start={4}'
+                               '&end={5}'.format(self.ssl, self.host,
+                                                 self.port, self.urlbase,
+                                                 start, end),
                 headers={'X-Api-Key': self.apikey}, timeout=10)
         except OSError:
             _LOGGER.warning("Host %s is not available", self.host)
-            self._state = None
+            self._state = 'Offline'
             return
 
-        if res.status_code == 200:
+        if api.status_code == 200:
+            self._state = 'Online'
             if self.days == 1:
-                self.data = list(filter(lambda
-                    x: x['airDate'][:-10] == str(start), res.json()))[:self.max_items]
-            else: self.data = res.json()[:self.max_items]
+                self.data = list(filter(lambda x: x['airDate'][:-10] == str(
+                    start), api.json()))[:self.max_items]
+            else: self.data = api.json()[:self.max_items]
+        else:
+            self._state = 'Offline'
 
 def get_date(zone, offset=0):
     """Get date based on timezone and offset of days."""
-    return datetime.date(datetime.fromtimestamp(time.time() + 86400 * offset, tz=zone))
+    return datetime.date(datetime.fromtimestamp(
+        time.time() + 86400 * offset, tz=zone))
+
+
+def days_until(date, tz):
+    from pytz import utc
+    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+    date = str(date.replace(tzinfo=utc).astimezone(tz))[:10]
+    date = time.strptime(date, '%Y-%m-%d')
+    date = time.mktime(date)
+    now = datetime.now().strftime('%Y-%m-%d')
+    now = time.strptime(now, '%Y-%m-%d')
+    now = time.mktime(now)
+    return int((date - now) / 86400)
